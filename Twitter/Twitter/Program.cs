9bc55c68558e5387108088d6784e2bbad3aaf3a1
Zach <zachcanada23@gmail.com>
2018-11-28 12:39:38 -0600

    Added Twitter Program

---
+++ Twitter/Twitter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Twitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int maxText = 140;
+
+
+
+
+            WriteLine("Enter Message Here>");
+            message = ReadLine();
+
+            if(message.Length> maxText)
+            {
+                WriteLine("To many characters, max is 140");
+            }
+            else if(message.Length< maxText)
+            {
+                WriteLine("Approved");
+            }
+        }
+    }
+}
