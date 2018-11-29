50b6780a16fe963f7616fabf57e3b707a8ab15ee
Zach <zachcanada23@gmail.com>
2018-11-29 13:09:09 -0600

    Added Swap Program

---
+++ SwapProgram/SwapProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace SwapProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 34, second = 712;
+            Write("Before swap first is {0} ", first);
+            WriteLine("and second is {0} ", second);
+            Swap(ref first, ref second);
+            Write("After swap first is {0} ", first);
+            WriteLine("and second is {0} ", second);
+        }
+        private static void Swap(ref int one, ref int two)
+        {
+            int temp;
+            temp = one;
+            one = two;
+            two = temp;
+        }
+
+    }
+}
